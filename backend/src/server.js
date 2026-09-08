bf8d3805d8354be9975c7c9c6b2d771333b90425
Sai Ganesh <ganeshrejeti1@gmail.com>
2026-09-08 10:57:54 +0530

    The cancelling subscription CRON job workign

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,18 @@ require ("dotenv").config();
 const app = require("./app");
 
 const connectDatabase = require("./config/database");
-
+const {expireCanceledSubscriptions} = require("./jobs/subscription.job")
 const PORT = 5000;
 
 //Starting the Database using the fuction which leads to config/database.js
 const startServer = async()=>{
     await connectDatabase();
+    await expireCanceledSubscriptions();//making it to run quickly after starting the server 
+
+    //making it run once for every 60 secs
+    setInterval(()=> {
+      expireCanceledSubscriptions();
+    }, 60*1000)
 }
 
 app.listen(PORT, () => {
