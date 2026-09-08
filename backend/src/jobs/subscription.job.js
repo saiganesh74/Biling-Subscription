e7c6b335d1309e93d4ce8ad0b5ef69b1e13e002f
Sai Ganesh <ganeshrejeti1@gmail.com>
2026-09-08 15:45:14 +0530

    Subscription renewal added for months and yeas

--- backend/src/jobs/subscription.job.js
+++ backend/src/jobs/subscription.job.js
@@ -1,3 +1,4 @@
+const Plan = require("../models/plan.model");
 const Subscription = require("../models/subscription.model");
 
 const expireCanceledSubscriptions = async () => {
@@ -20,4 +21,43 @@ const expireCanceledSubscriptions = async () => {
     }
 }
 
-module.exports = {expireCanceledSubscriptions};
+const renewSubscriptions = async () => {
+    try {
+        const subscriptions = await Subscription.find({
+            status: "ACTIVE",
+            cancelAtPeriodEnd: false,
+            currentPeriodEnd: {
+                $lte: new Date()
+            }
+        });
+
+        for (const subscription of subscriptions) {
+            const plan = await Plan.findById(subscription.planId);
+
+            if (!plan) {
+                continue;
+            }
+            subscription.currentPeriodStart = new Date(subscription.currentPeriodEnd);
+            subscription.currentPeriodEnd = new Date(subscription.currentPeriodStart);
+            if (plan.interval === "month") {
+                subscription.currentPeriodEnd.setMonth(
+                    subscription.currentPeriodEnd.getMonth() + 1
+                );
+            }
+            if (plan.interval === "year") {
+                subscription.currentPeriodEnd.setFullYear(
+                    subscription.currentPeriodEnd.getMonth() + 1
+                );
+            };
+            await subscription.save();
+        }
+        console.log(`Renewed Subscriptions : ${subscriptions.length}`)
+    } catch (e) {
+        console.error(
+            "Failed to renew subscriptions:",
+            e.message
+        );
+    }
+}
+
+module.exports = { expireCanceledSubscriptions, renewSubscriptions };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,23 @@
-require ("dotenv").config();
+require("dotenv").config();
 
 const app = require("./app");
 
 const connectDatabase = require("./config/database");
-const {expireCanceledSubscriptions} = require("./jobs/subscription.job")
+const { expireCanceledSubscriptions, renewSubscriptions } = require("./jobs/subscription.job")
 const PORT = 5000;
 
 //Starting the Database using the fuction which leads to config/database.js
-const startServer = async()=>{
-    await connectDatabase();
-    await expireCanceledSubscriptions();//making it to run quickly after starting the server 
+const startServer = async () => {
+  await connectDatabase();
+  await expireCanceledSubscriptions();//making it to run quickly after starting the server 
+  await renewSubscriptions();
 
-    //making it run once for every 60 secs
-    setInterval(()=> {
-      expireCanceledSubscriptions();
-    }, 60*1000)
+
+  //making it run once for every 60 secs
+  setInterval(() => {
+    expireCanceledSubscriptions();
+    renewSubscriptions();
+  }, 60 * 1000)
 }
 
 app.listen(PORT, () => {
